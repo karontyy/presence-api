a057ce18d97f4fa382adbdc1e7206c46344ea826
bersoncrios <bersondev@gmail.com>
2022-03-08 11:50:09 -0300

    fazendo inner join para buscar o produto do pedido

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -8,16 +8,26 @@ router.get('/', (req, res, next) => {
         if (error) { return res.status(500).send({ error: error }) }
 
         conn.query(
-            'SELECT * FROM pedidos;',
+            `SELECT pedidos.id_pedido,
+                    pedidos.quantidade, 
+                    produtos.id_produto, 
+                    produtos.nome, 
+                    produtos.preco 
+                FROM pedidos 
+          INNER JOIN produtos 
+                  ON produtos.id_produto = pedidos.id_produto;`,
             (error, resultado, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
                 const response = {
-                    quantidade: resultado.length,
                     pedidos: resultado.map(pedido => {
                         return {
                             id_pedido: pedido.id_pedido,
                             quantidade: pedido.quantidade,
-                            id_produto: pedido.id_produto,
+                            produto: {
+                                id_produto: pedido.id_produto,
+                                nome: pedido.nome,
+                                preco: pedido.preco
+                            },
                             request: {
                                 tipo: 'GET',
                                 descricao: '',
