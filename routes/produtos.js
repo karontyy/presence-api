9adadc3d8c0f9fd8e89992c67b8383105786e8ab
bersoncrios <bersondev@gmail.com>
2022-03-08 16:45:21 -0300

    criando middleware de login para token jwt e usando

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const multer = require('multer');
+const login = require('../middleware/login')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -89,8 +90,8 @@ router.get('/:id_produto', (req, res, next) => {
 });
 
 // SALVA UM PRODUTO
-router.post('/', upload.single('imagem_produto'), (req, res, next) => {
-    console.log(req.file);
+router.post('/', login.obrigatorio, upload.single('imagem_produto'), (req, res, next) => {
+    console.log(req.usuario);
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
 
@@ -124,7 +125,7 @@ router.post('/', upload.single('imagem_produto'), (req, res, next) => {
 
 
 // DELETA UM PRODUTO
-router.delete('/:id_produto', (req, res, next) => {
+router.delete('/:id_produto', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
 
@@ -152,7 +153,7 @@ router.delete('/:id_produto', (req, res, next) => {
 });
 
 // ALTERA UM PRODUTO
-router.patch('/:id_produto', (req, res, next) => {
+router.patch('/:id_produto', login.obrigatorio, (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) }
 
