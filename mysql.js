8cda3afddd6267e35ea58763879807def59c6185
bersoncrios <bersondev@gmail.com>
2022-03-10 16:39:52 -0300

    add banco remoto

--- mysql.js
+++ mysql.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 var pool = mysql.createPool({
-    "user"     :    "root",
-    "password" :    "root",
-    "database" :    "presenteApi",
+    "user"     :    "b95f0d916ff53b",
+    "password" :    "90e668d5",
+    "database" :    "heroku_9e7cb4e3b72c115",
     "host"     :    process.env.MYSQL_HOST,
     "port"     :    process.env.MYSQL_PORT
 });
