34f1962d819a72df4f8d7423566baf5b923ac59a
bersoncrios <bersondev@gmail.com>
2022-03-15 10:34:15 -0300

    ajuste de deploy

--- routes/professor.routes.js
+++ routes/professor.routes.js
@@ -7,4 +7,5 @@ const ProfessorController = require('../controllers/professor.controllers')
 router.post('/cadastro', ProfessorController.cadastroDeUsuarios);
 router.post('/login', ProfessorController.loginDeUsuario);
 router.get('/dadosCadastrais/:id_professor', login.obrigatorio, ProfessorController.buscaDadosProfessor);
+
 module.exports = router;
